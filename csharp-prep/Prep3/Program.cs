b49b616d840113876c68dbf92c3cf588b2fbcfe0
Brenden <joh22047@byui.edu>
2025-05-01 15:20:30 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess;
+        
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+                Console.WriteLine("Lower");
+            else 
+                Console.WriteLine("Higher");
+            
+        } while (guess != number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
